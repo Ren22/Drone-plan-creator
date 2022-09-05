f1de1b4aa7462b0a396c19e5035855a80a1c3b92
Ren22 <ngm.renat@gmail.com>
2022-09-05 02:11:48 +0200

    feat: removed a non working button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { PlanList } from '../PlanList';
 import { PlanSaver } from '../PlanSaver';
 import { Map } from '../Map';
 import './Layout.css';
-import { Button } from '../Button';
+import {} from '../../features/plans/planSlice';
 
 const Layout = () => {
-  const [isMarkerAddingMode, setIsMarkerAddingMode] = useState(false);
-  const enableAddMarkerMode = () => {
-    setIsMarkerAddingMode(!isMarkerAddingMode);
-  };
   return (
     <>
       <div className="content">
         <Map />
         <div>
-          <Button onClick={enableAddMarkerMode}>
-            {`${isMarkerAddingMode ? 'Stop' : 'Start'} plan creation`}
-          </Button>
           <PlanSaver />
           <PlanList />
         </div>
